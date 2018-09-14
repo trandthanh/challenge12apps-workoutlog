fc4c7d5dadd8a05a63b4bf501664d0812b2c92df
Dang-Thanh Tran <trandgthanh@gmail.com>
2018-09-14 16:09:17 +0200

    Edit & Destroy Actions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -22,15 +22,19 @@ class WorkoutsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @workout.update(workout_params)
+      redirect_to @workout
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @workout.destroy
+    redirect_to root_path
   end
 
   private
